d90f7bfdd5640cd41b49b39fa50d6e85bae091cb
dan1aa <burmeidan@gmail.com>
2021-10-22 15:07:49 +0300

    validation

--- routes/deleteUser.js
+++ routes/deleteUser.js
@@ -4,11 +4,17 @@ const userList = require('../users.json')
 
 router.delete('/users/:userId', (req, res) => {
     let { userId } = req.params;
-    let updatedUserList = userList.filter(user => user.id !== userId)
-    fs.writeFile('users.json', JSON.stringify(updatedUserList), err => {
-        if (err) throw new Error(err)
-    })
-    res.end(JSON.stringify(updatedUserList))
+    let currentUser = userList.find(user => user.id === userId)
+    if(currentUser) {
+        let updatedUserList = userList.filter(user => user.id !== userId)
+        fs.writeFile('users.json', JSON.stringify(updatedUserList), err => {
+            if (err) throw new Error(err)
+        })
+        res.end(JSON.stringify(updatedUserList))
+    }
+    else {
+        res.status(404).end('User not found')
+    }
 })
 
 module.exports = router;
--- routes/editUser.js
+++ routes/editUser.js
@@ -5,12 +5,17 @@ const userList = require('../users.json')
 router.patch('/users/:userId', (req, res) => {
     let { userId } = req.params;
     let currentUser = userList.find(user => user.id === userId)
-    currentUser.name = req.body?.name || currentUser.name
-    currentUser.age = req.body?.age || currentUser.age
-    fs.writeFile('users.json', JSON.stringify(userList), err => {
-        if (err) throw new Error(err)
-    })
-    res.end(JSON.stringify(currentUser))
+    if (currentUser) {
+        currentUser.name = req.body?.name || currentUser.name
+        currentUser.age = req.body?.age || currentUser.age
+        fs.writeFile('users.json', JSON.stringify(userList), err => {
+            if (err) throw new Error(err)
+        })
+        res.end(JSON.stringify(currentUser))
+    }
+    else {
+        res.status(404).end('User not found')
+    }
 })
 
 module.exports = router;
